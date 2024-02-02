97e774a3e91322c808c7bcd0a203aee0c19cab3e
iRaYzEn <abbassgame335463@gmail.com>
2024-02-02 18:30:08 +0300

    modifing nvim

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,14 +1,14 @@
-require('gruvbox').setup({
+-- gruvbox rose-pine
+require("gruvbox").setup({
     disable_background = true
 })
 
-function ColorMyPencils(color) 
-	color = color or "gruvbox"
-	vim.cmd.colorscheme(color)
+function ColorMyPencils(color)
+    color = color or "gruvbox"
+    vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
--- .config/nvim/lua/irayzen/set.lua
+++ .config/nvim/lua/irayzen/set.lua
@@ -28,10 +28,12 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 999
+vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
 
 vim.opt.ignorecase = true

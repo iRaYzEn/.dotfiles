bb6888490ae7ac0bc7e5de9e354abb5f88d5e7a6
iRaYzEn <abbassgame335463@gmail.com>
2024-04-20 01:33:41 +0300

    adding things to i3 and nvim

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -4,7 +4,7 @@ require("rose-pine").setup({
 })
 
 function ColorMyPencils(color)
-    color = color or "rose-pine"
+    color = color or "rose-pine-moon"
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })

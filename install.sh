9147317801fbdebee6945d4af59bf619be1490f7
rayzen <abbassgame335463@gmail.com>
2024-08-10 11:43:31 +0300

    adding brave to the installer script

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-sudo apt install make gcc ripgrep unzip git xclip i3 zsh tmux pavucontrol shutter compton
+sudo apt update && sudo apt upgrade
+sudo apt install make gcc ripgrep unzip git xclip i3 zsh tmux pavucontrol shutter compton curl
 
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser

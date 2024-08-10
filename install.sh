645cdaf47200534579c43fc5f3bc8c2671e17f80
rayzen <abbassgame335463@gmail.com>
2024-08-10 11:34:03 +0300

    installer script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt install make gcc ripgrep unzip git xclip i3 zsh tmux pavucontrol shutter compton
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete

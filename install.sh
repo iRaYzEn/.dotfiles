0a37f88042a0b39bae86625c63234e168f474b3d
rayzen <abbassgame335463@gmail.com>
2024-08-10 11:59:52 +0300

    dev related

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update && sudo apt upgrade
-sudo apt install make gcc ripgrep unzip git xclip i3 zsh tmux pavucontrol shutter compton curl ffmpeg
+sudo apt install make gcc ripgrep unzip git xclip i3 zsh tmux pavucontrol shutter compton curl ffmpeg discord
 
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
@@ -16,3 +16,22 @@ sudo apt install python3
 sudo apt install pip3 
 pip3 install yt-dlp
 pip3 install spotdl
+
+
+# spotify 
+curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+
+# node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install latest
+
+# nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
+# golang
+curl -O https://go.dev/dl/go1.22.6.src.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.6.linux-amd64.tar.gz

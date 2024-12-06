c5d22738838b8a9783d8cbc6797485826fd6637e
iRaYzEn <79583015+iRaYzEn@users.noreply.github.com>
2024-12-06 12:54:52 +0300

    install.sh

---
+++ install.sh
@@ -0,0 +1,30 @@
+echo "updating package list"
+sudo apt update
+
+
+echo "installing progams"
+sudo apt install -y \
+    tmux \
+    fzf \
+    i3 \
+    dmenu \
+    nitrogen \
+    curl \
+
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
+
+curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt--get update && sudo apt-get install spotify-client
+
+echo "cleaning up..."
+sudo apt autoremove -y
+sudo apt autoclean
+
+echo "complete"
+
+

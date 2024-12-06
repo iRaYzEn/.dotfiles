dfdbd6c964dc073154443047c027fe05ac220167
iRaYzEn <79583015+iRaYzEn@users.noreply.github.com>
2024-12-06 13:09:37 +0300

    adding nvim to the scrip

--- install.sh
+++ install.sh
@@ -21,6 +21,12 @@ curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt--get update && sudo apt-get install spotify-client
 
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+sudo ln -sf /opt/nvim-linux64/bin/nvim /usr/bin/nvim
+rm nvim-linux64.tar.gz  # Clean up tar.gz file
+
 echo "cleaning up..."
 sudo apt autoremove -y
 sudo apt autoclean

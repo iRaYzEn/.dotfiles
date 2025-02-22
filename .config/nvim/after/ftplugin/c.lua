1c5529fdddbca69f145e63c4db5a172943a3dd7f
iRaYzEn <79583015+iRaYzEn@users.noreply.github.com>
2025-02-22 13:01:59 +0300

    thing

---
+++ .config/nvim/after/ftplugin/c.lua
@@ -0,0 +1,4 @@
+-- away to over right some options in certain files after running the entire configuration if needed
+-- local set = vim.opt_local
+
+-- set.shiftwidth = 2
--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -8,8 +8,8 @@ return {
         function ColorMyPencils(color)
             -- color = color or "rose-pine-main"
             color = color or "gruvbox"
-            -- color = color or "tokyonight"
             -- color = color or "catppuccin"
+            -- color = color or "tokyonight"
             vim.cmd.colorscheme(color)
             vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
             vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })

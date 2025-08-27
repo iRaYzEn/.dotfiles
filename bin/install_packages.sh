6a713c6d5b3fb5fc352b5dacef02c6f9a063ca7b
iRaYzEn <irayzen.me@gmail.com>
2025-08-27 11:31:52 +0300

    everything is fixed now

--- bin/install_packages.sh
+++ bin/install_packages.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-yes | sudo pacman -S $(cat $HOME/.dotfiles/software.txt)
+yes | sudo pacman -S $(cat $HOME/.dotfiles/packages/pacman.txt)
 
 
-# this thing is useful 
-#sudo ln -sf /usr/bin/ghostty /usr/bin/xterm
 
 if [ -f $HOME/dl/git ];then
     cd $HOME/dl/git/ && git clone https://github.com/morganamilo/paru
-    makepkg -si
+    cd $HOME/dl/git/paru && makepkg -si
 else
     mkdir -p $HOME/dl/git
     cd $HOME/dl/git/ && git clone https://github.com/morganamilo/paru
-    cd $HOME/dl/git/paru/ && yes | sudo makepkg -si
+    cd $HOME/dl/git/paru/ && makepkg -si
 fi
--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 
-sudo su
 bash $HOME/.dotfiles/bin/install_packages.sh
 
 sudo chsh -s $(which zsh)
 stow .
+sudo pacman -S ghostty
+# i3-sensible-terminal don't support ghostty so just link ghostty to xterm it is fine i guess
+sudo ln -sf /usr/bin/ghostty /usr/bin/xterm
+

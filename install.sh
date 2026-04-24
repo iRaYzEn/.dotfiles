f77946835e1ca243c881e0fab15a5c54f28a92e9
iRaYzEn <79583015+iRaYzEn@users.noreply.github.com>
2026-04-24 16:33:07 +0300

    adding c version 23 in the clangd config file

--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@ bash $HOME/.dotfiles/bin/install_packages.sh
 
 sudo chsh -s $(which zsh)
 stow .
+
+# to make the gtk application dark theme like evince for example
+gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
+gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+
 sudo pacman -S ghostty
 # i3-sensible-terminal don't support ghostty so just link ghostty to xterm it is fine i guess
 sudo ln -sf /usr/bin/ghostty /usr/bin/xterm
